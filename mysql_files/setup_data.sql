03b5e7313413b65539aad8cbb33be03ab9be5e5d
azalac <zalac.austin@gmail.com>
2019-04-17 16:36:54 -0400

    Removed old mysql files (moved to sql server)

--- mysql_files/setup_data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO people(HCN, lastName, firstName, mInitial, dateBirth, sex, HouseID) VALUES('0123456789AB', 'smith', 'bob', 'c', '1997-11-20', 'M', NULL);
-INSERT INTO people(HCN, lastName, firstName, mInitial, dateBirth, sex, HouseID) VALUES('0123456789AB', 'smithert', 'bobert', 'c', '1997-11-21', 'M', NULL);
-
-CALL CreateAppointment(2005, 5, 25, 3, 1, 2)
