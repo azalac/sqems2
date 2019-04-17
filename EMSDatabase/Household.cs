2b09c2f24c0b5867662f6723df144a4aa020249f
azalac <zalac.austin@gmail.com>
2019-04-17 16:27:12 -0400

    Changed head of house storage (new table) and added comments

--- EMSDatabase/Household.cs
+++ EMSDatabase/Household.cs
@@ -3,84 +3,109 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
 
 namespace EMSDatabase
 {
-    public class HouseholdFactory : DatabaseRowFactoryBase<Household, int>
+    /// <summary>
+    /// Finds and creates households
+    /// </summary>
+    public class HouseholdFactory : DatabaseRowFactoryBase<Household>
     {
         public HouseholdFactory(QueryFactory queryFactory) : base(queryFactory)
         {
             ignoreExtras = false;
         }
 
+        /// <summary>
+        /// Finds a house by its id
+        /// </summary>
         public Household Find(int ID)
         {
             return Find("SELECT * FROM Household WHERE ID = @0", ID);
         }
 
+        /// <summary>
+        /// Finds or creates a house with the given information
+        /// </summary>
+        /// <returns></returns>
         public Household FindOrCreate(string Address1 = null, string Address2 = null, string City = null,
             string Province = null, string PhoneNumber = null)
         {
-            DynamicSQLQuery predicate = new DynamicSQLQuery();
-
-            predicate.TryAddCondition("AddressLine1 = {0}", Address1);
-            predicate.TryAddCondition("AddressLine2 = {0}", Address2);
-            predicate.TryAddCondition("City = {0}", City);
-            predicate.TryAddCondition("Province = {0}", Province);
-            predicate.TryAddCondition("PhoneNumber = {0}", PhoneNumber);
-
-            var (predicate_str, predicate_obj) = predicate.Get();
-
-            Household household = Find(predicate_str, predicate_obj);
+            Household household = Find(Address1, Address2, City, Province, PhoneNumber);
 
             if(household == null)
             {
                 Create(Address1, Address2, City, Province, PhoneNumber);
-                household = FindImpl(Address1, Address2, City, Province, PhoneNumber);
+                household = Find(Address1, Address2, City, Province, PhoneNumber);
             }
 
             return household;
         }
         
-        private void Create(string Address1 = null, string Address2 = null, string City = null,
-            string Province = null, string PhoneNumber = null)
-        {
-            using(MySqlCommand cmd = queryFactory.CreateQuery("INSERT INTO Household (addressLine1, addressLine2, city, province, numPhone) VALUES (@0, @1, @2, @3, @4)",
-                Address1, Address2, City, Province, PhoneNumber))
-            {
-                int affected = cmd.ExecuteNonQuery();
-            }
-        }
-
-        private Household FindImpl(string Address1 = null, string Address2 = null, string City = null,
+        /// <summary>
+        /// Finds the first household with the given information
+        /// </summary>
+        public Household Find(string Address1 = null, string Address2 = null, string City = null,
             string Province = null, string PhoneNumber = null)
         {
 
-            DynamicSQLQuery predicate = new DynamicSQLQuery();
+            SqlWhereClauseBuilder predicate = new SqlWhereClauseBuilder();
 
             predicate.TryAddCondition("AddressLine1 = {0}", Address1);
             predicate.TryAddCondition("AddressLine2 = {0}", Address2);
             predicate.TryAddCondition("City = {0}", City);
             predicate.TryAddCondition("Province = {0}", Province);
-            predicate.TryAddCondition("PhoneNumber = {0}", PhoneNumber);
+            predicate.TryAddCondition("numPhone = {0}", PhoneNumber);
 
             var (predicate_str, predicate_obj) = predicate.Get();
             
-            return Find("SELECT addressLine1, addressLine2, city, province, numPhone, headOfHouse FROM Household WHERE " + predicate_str, predicate_obj);
+            return Find("SELECT * FROM Household WHERE " + predicate_str, predicate_obj);
         }
 
-        protected override Household CreateObject(MySqlDataReader reader)
+        /// <summary>
+        /// Creates a household with the given information, does not return it.
+        /// </summary>
+        private void Create(string Address1 = null, string Address2 = null, string City = null,
+            string Province = null, string PhoneNumber = null)
+        {
+            SqlInsertBuilder insertBuilder = new SqlInsertBuilder()
+            {
+                Table = "Household"
+            };
+
+            insertBuilder.TryAddColumn("addressLine1", Address1);
+            insertBuilder.TryAddColumn("addressLine2", Address2);
+            insertBuilder.TryAddColumn("city", City);
+            insertBuilder.TryAddColumn("province", Province);
+            insertBuilder.TryAddColumn("numPhone", PhoneNumber);
+
+            using (SqlCommand cmd = queryFactory.CreateQuery(insertBuilder.GetQuery(), insertBuilder.GetValues()))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        protected override Household CreateObject(SqlDataReader reader)
         {
             return DatabaseRowObjectAdapter.Fill(new Household(queryFactory), reader);
         }
     }
 
+    /// <summary>
+    /// Represents a household
+    /// </summary>
     public class Household
     {
-        public int ID, HeadOfHouse;
-        public string AddressLine1, AddressLine2, City, Province, PhoneNumber;
+        /// <summary>
+        /// Gets or sets the head of house (Person ID)
+        /// </summary>
+        public int HeadOfHouse { get => GetHeadOfHouse(); set => SetHeadOfHouse(value); }
 
+        public readonly int ID;
+
+        public readonly string AddressLine1, AddressLine2, City, Province, PhoneNumber;
+        
         private QueryFactory queryFactory;
 
         public Household(QueryFactory queryFactory)
@@ -88,16 +113,40 @@ namespace EMSDatabase
             this.queryFactory = queryFactory;
         }
 
-        public void SetHeadOfHouse(int hohId)
+        /// <summary>
+        /// Sets the head of house.
+        /// </summary>
+        /// <param name="hohId">Sets the head of house, with null being none</param>
+        /// <exception cref="ArgumentException">If the given person id is not found</exception>
+        public void SetHeadOfHouse(int? hohId)
         {
-            using (MySqlCommand cmd = queryFactory.CreateQuery("CALL SetHouseholdHead(@0, @1)", ID, hohId))
+            // Delete the previous house head (if there was one)
+            using (SqlCommand deleter = queryFactory.CreateQuery("DELETE FROM HouseHead WHERE HouseID = @0", ID))
             {
-                int status = (int)cmd.ExecuteScalar();
+                deleter.ExecuteNonQuery();
+            }
 
-                if(status == 0)
+            if (hohId != null)
+            {
+                // Inserts a new house head if it's not being removed
+                using (SqlCommand cmd = queryFactory.CreateQuery("INSERT INTO HouseHead (HouseID, PersonID) VALUES (@0, @1)", ID, hohId))
                 {
-                    throw new ArgumentException("Person with ID " + hohId + " does not exist");
+                    if (cmd.ExecuteNonQuery() == 0)
+                    {
+                        throw new ArgumentException("Invalid head of house id " + hohId);
+                    }
                 }
+            }
+        }
+
+        /// <summary>
+        /// Gets the head of house
+        /// </summary>
+        public int GetHeadOfHouse()
+        {
+            using (SqlCommand cmd = queryFactory.CreateQuery("SELECT PersonID FROM HouseHead WHERE HouseID = @0", ID))
+            {
+                return (int)cmd.ExecuteScalar();
             }
         }
     }
