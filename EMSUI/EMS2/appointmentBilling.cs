35830e943b17e0fd2b141db80c065562822a66f0
billyParmenter <bparmenter6387@conestogac.on.ca>
2019-04-19 13:09:33 -0400

    Added comments

--- EMSUI/EMS2/appointmentBilling.cs
+++ EMSUI/EMS2/appointmentBilling.cs
@@ -13,19 +13,29 @@ namespace EMS2
 {
     public partial class appointmentBilling : Form
     {
+        // Factories being used in the UI
         private static QueryFactory queryFactory = new QueryFactory();
         private BillableProcedureFactory billableProcedureFactory = new BillableProcedureFactory(queryFactory);
         private BillingCodeFactory billingCodeFactory = new BillingCodeFactory(queryFactory);
         private static PeopleFactory peopleFactory = new PeopleFactory(queryFactory);
         private static AppointmentFactory appointmentFactory = new AppointmentFactory(queryFactory, peopleFactory);
         private AppointmentPatientFactory appointmentPatientFactory = new AppointmentPatientFactory(queryFactory, appointmentFactory, peopleFactory);
-        private AppointmentPatient appointmentPatient = new AppointmentPatient(appointmentFactory, peopleFactory);
 
-
+        // Appointment selected from the main form
         private Appointment appointment = Form1.SelectedAppointment;
+        
+       
+        private AppointmentPatient appointmentPatient = new AppointmentPatient(appointmentFactory, peopleFactory);
         private List<BillableProcedure> billableProcedures = new List<BillableProcedure>();
         private List<BillingCode> billingCodes = new List<BillingCode>();
 
+
+
+
+
+        /// <summary>
+        /// Constructor for form
+        /// </summary>
         public appointmentBilling()
         {
             InitializeComponent();
@@ -36,6 +46,7 @@ namespace EMS2
 
             billableProcedures = billableProcedureFactory.GetBillableProcedures(appointmentPatient);
 
+            // Converts the found list of billable procedures to a list of billing codes 
             procedureToCode();
 
             billingCodeBox.DataSource = billingCodes;
@@ -43,15 +54,32 @@ namespace EMS2
 
         }
 
-        private void procedureToCode()
+
+
+
+
+        /// <summary>
+        /// When the finish button is clicked update the  billableProcedures
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Finish_Click(object sender, EventArgs e)
         {
-            foreach (BillableProcedure procedure in billableProcedures)
-            {
-                billingCodes.Add(billingCodeFactory.Find(procedure.CodeName));
-            }
+            billableProcedureFactory.SetBillableProcedures(appointmentPatient, billingCodes);
+
+            this.Close();
         }
 
-        private void addBillingCode_Click(object sender, EventArgs e)
+
+
+
+
+        /// <summary>
+        /// When the add button is clicked add the given code if it is found
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Add_Click(object sender, EventArgs e)
         {
             BillingCode code = billingCodeFactory.Find(billingCodeTB.Text);
             if (code != null)
@@ -59,28 +87,57 @@ namespace EMS2
                 billingCodes.Add(code);
             }
 
-            billingCodeBox.DataSource = null;
-            billingCodeBox.DataSource = billingCodes;
-            billingCodeBox.DisplayMember = "Display";
+
         }
 
-        private void removeBillingCode_Click(object sender, EventArgs e)
+
+
+
+
+        /// <summary>
+        /// When the remove button is clicked remove the selected code
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Remove_Click(object sender, EventArgs e)
         {
             if (billingCodeBox.SelectedIndex >= 0)
             {
                 billingCodes.RemoveAt(billingCodeBox.SelectedIndex);
-                billingCodeBox.DataSource = null;
-                billingCodeBox.DataSource = billingCodes;
-                billingCodeBox.DisplayMember = "Display";
+
+                UpdateList();
             }
         }
 
-        private void billingFinish_Click(object sender, EventArgs e)
-        {
-            AppointmentPatient appointmentPatient = appointmentPatientFactory.Find(appointment.ID);
-            billableProcedureFactory.SetBillableProcedures(appointmentPatient, billingCodes);
 
-            this.Close();
+
+
+
+        /// <summary>
+        /// add thebilling codes of the found billableProcedures to the listBox
+        /// </summary>
+        private void procedureToCode()
+        {
+            foreach (BillableProcedure procedure in billableProcedures)
+            {
+                billingCodes.Add(billingCodeFactory.Find(procedure.CodeName));
+
+                UpdateList();
+            }
+        }
+
+
+
+
+
+        /// <summary>
+        /// Updates the datasource for the listBox
+        /// </summary>
+        private void UpdateList()
+        {
+            billingCodeBox.DataSource = null;
+            billingCodeBox.DataSource = billingCodes;
+            billingCodeBox.DisplayMember = "Display";
         }
     }
 }
