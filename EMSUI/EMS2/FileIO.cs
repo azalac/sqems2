479b42dca128ae316b2ce012c9a9b0b5a28da2a2
billyParmenter <bparmenter6387@conestogac.on.ca>
2019-04-21 16:12:14 -0400

    Created functions for FileIO

--- EMSUI/EMS2/FileIO.cs
+++ EMSUI/EMS2/FileIO.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace EMS2
 {
     class FileIO
     {
+        private readonly string saveLocation = "../../../../monthlySummaries";
+
+
+        public void SaveToFile(string data, string file)
+        {
+            try
+            {
+                File.WriteAllText(string.Format("{0}/{1}.txt", saveLocation, file), data);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+
+
+        public string ReadFromFile(string file)
+        {
+            string data = null;
+
+            try
+            {
+                data = File.ReadAllText(string.Format("{0}/{1}.txt", saveLocation, file));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            return data;
+        }
     }
 }
