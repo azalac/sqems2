682c14fb3965aeda01fdad2d5137c6447a0ce2c2
billyParmenter <bparmenter6387@conestogac.on.ca>
2019-04-23 00:02:00 -0400

    Added picking file to reconsile

--- EMSUI/EMS2/FileIO.cs
+++ EMSUI/EMS2/FileIO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -23,6 +24,56 @@ namespace EMS2
             {
                 MessageBox.Show(e.Message);
             }
+        }
+
+
+
+        public string openFile()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            string filePath = string.Empty;
+            // = "Text File|*.txt|PNG File|*.png     ------------------ multiple file types?
+            openFile.Filter = "Text File|*.txt";
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFile.FileName;
+                //shows file name without path
+                //MessageBox.Show(openFile.SafeFileName);
+            }
+            return filePath;
+        }
+
+
+
+
+
+
+        public List<string> ReadReconcile(string path)
+        {
+            Regex regex = new Regex("^[0-9]{18}[a-zA-Z]{4}[0-9]{14}[a-zA-Z]{4}$");
+            List<string> temp = new List<string>();
+            List<string> data = new List<string>();
+
+
+            try
+            {
+                temp = File.ReadLines(path).ToList();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            foreach (string s in temp)
+            {
+                if (regex.Match(s).Success)
+                {
+                    data.Add(s);
+                }
+            }
+
+            return data;
         }
 
 
